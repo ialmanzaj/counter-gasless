45660e43eba802d21f95edd03638ceb9e3779803
Isaac Almanza <isaac.almanza19@gmail.com>
2024-03-06 22:58:36 -0500

    fix: fix lint errors

--- test/GaslessTransferToken.t.sol
+++ test/GaslessTransferToken.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.23;
 
 import { PRBTest } from "@prb/test/src/PRBTest.sol";
-import { console2 } from "forge-std/src/console2.sol";
 import { StdCheats } from "forge-std/src/StdCheats.sol";
 
 import { GaslessTransferToken } from "../src/GaslessTransferToken.sol";
